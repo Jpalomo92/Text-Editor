4f32fec0d80a79d5ea7d30d52d76c8ca39ffc29c
Jpalomo <Jpalomo92@gmail.com>
2024-04-08 13:14:18 -0500

    added a console log to the app installed function - not sure if I need to add anything else

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -10,8 +10,11 @@ window.addEventListener('beforeinstallprompt', (event) => {
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    butInstall.setAttribute('disabled', false)
+    butInstall.setAttribute('disabled', false);
+    butInstall.textContent('Installed');
 });
 
 // TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+window.addEventListener('appinstalled', (event) => {
+    console.log('This application was installed!');
+});
