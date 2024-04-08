5af9a91fad253a515aba0eca72fe91b11a0521fe
Jpalomo <Jpalomo92@gmail.com>
2024-04-08 13:05:47 -0500

    created the set attribute to the button install const - set to disabled and false

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -9,7 +9,9 @@ window.addEventListener('beforeinstallprompt', (event) => {
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+butInstall.addEventListener('click', async () => {
+    butInstall.setAttribute('disabled', false)
+});
 
 // TODO: Add an handler for the `appinstalled` event
 window.addEventListener('appinstalled', (event) => {});
