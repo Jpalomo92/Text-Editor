dda5e55883a1ccc700e5d9f705438998f59e3017
Jpalomo <Jpalomo92@gmail.com>
2024-04-08 22:12:36 -0500

    added the toggle and deffered prompts regarding the button to fix bug - button wasnt working

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -4,14 +4,27 @@ const butInstall = document.getElementById('buttonInstall');
 // TODO: Add an event handler to the `beforeinstallprompt` event
 window.addEventListener('beforeinstallprompt', (event) => {
     event.preventDefault();
-    butInstall.style.visibility = 'visible';
-    butInstall.textContent = 'Install'
+    //butInstall.style.visibility = 'visible';
+    //butInstall.textContent = 'Install!'
+
+    window.deferredPrompt = event;
+
+    // Remove the hidden class from the button.
+    butInstall.classList.toggle('hidden', false);
 });
 
 // TODO: Implement a click event handler on the `butInstall` element
 butInstall.addEventListener('click', async () => {
-    butInstall.setAttribute('disabled', false);
-    butInstall.textContent('Installed');
+    const eventPrompt = window.defferedPrompt;
+    if (!eventPrompt) {
+        return;
+    }
+
+    eventPrompt.prompt();
+    window.defferedPrompt = null
+    butInstall.classList.toggle('hidden', true);
+    // butInstall.setAttribute('disabled', false);
+    // butInstall.textContent('Installed');
 });
 
 // TODO: Add an handler for the `appinstalled` event
